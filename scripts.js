4c0f21921c983134b890d15998f0004518d43b0e
Masen Matthews <masenmatthews@gmail.com>
2018-10-10 13:37:49 -0700

    finish incorporating initial UI logic

--- scripts.js
+++ scripts.js
@@ -61,6 +61,15 @@
     }
   }
 
+  const rootReducer = this.Redux.combineReducers({
+    currentSongId: songChangeReducer,
+    songsById: lyricChangeReducer
+  });
+
+  // REDUX STORE
+  const { createStore } = Redux;
+  const store = createStore(rootReducer);
+
   // JEST TESTS + SETUP
   const { expect } = window;
 
@@ -104,33 +113,81 @@
 
   expect(songChangeReducer(initialState, { type: 'CHANGE_SONG', newSelectedSongId: 1 })).toEqual(1);
 
-  // REDUX STORE
-  const { createStore } = Redux;
-  const store = createStore(lyricChangeReducer);
+  expect(rootReducer(initialState, { type: null })).toEqual(initialState);
+
+  expect(store.getState().currentSongId).toEqual(songChangeReducer(undefined, { type: null }));
+  expect(store.getState().songsById).toEqual(lyricChangeReducer(undefined, { type: null }));
+
 
   // RENDERING STATE IN DOM
-  // const renderLyrics = () => {
-  //   const lyricsDisplay = document.getElementById('lyrics');
-  //   while (lyricsDisplay.firstChild) {
-  //     lyricsDisplay.removeChild(lyricsDisplay.firstChild);
-  //   }
-  //   const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
-  //   const renderedLine = document.createTextNode(currentLine);
-  //   document.getElementById('lyrics').appendChild(renderedLine);
-  // }
-  //
-  // window.onload = function() {
-  //   renderLyrics();
-  // }
-  //
-  // // CLICK LISTENER
-  // const userClick = () => {
-  //   if (store.getState().arrayPosition === store.getState().songLyricsArray.length - 1) {
-  //     store.dispatch({ type: 'RESTART_SONG' } );
-  //   } else {
-  //     store.dispatch({ type: 'NEXT_LYRIC' } );
-  //   }
-  // }
-  //
-  // // SUBSCRIBE TO REDUX STORE
-  // store.subscribe(renderLyrics);
+  const renderLyrics = () => {
+    const lyricsDisplay = document.getElementById('lyrics');
+    while (lyricsDisplay.firstChild) {
+      lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+    }
+
+    if (store.getState().currentSongId) {
+      const currentLine = document.createTextNode(store.getState().songsById[store.getState().currentSongId].songArray[store.getState().songsById[store.getState().currentSongId].arrayPosition]);
+      document.getElementById('lyrics').appendChild(currentLine);
+    } else {
+      const selectSongMessage = document.createTextNode("Select a song from the menu above to sing along!");
+      document.getElementById('lyrics').appendChild(selectSongMessage);
+    }
+  }
+
+  const renderSongs = () => {
+    console.log('renderSongs method successfully fired!');
+    console.log(store.getState());
+    const songsById = store.getState().songsById;
+    for (const songKey in songsById) {
+      const song = songsById[songKey]
+      const li = document.createElement('li');
+      const h3 = document.createElement('h3');
+      const em = document.createElement('em');
+      const songTitle = document.createTextNode(song.title);
+      const songArtist = document.createTextNode(' by ' + song.artist);
+      em.appendChild(songTitle);
+      h3.appendChild(em);
+      h3.appendChild(songArtist);
+      h3.addEventListener('click', function() {
+        selectSong(song.songId);
+      });
+      li.appendChild(h3);
+      document.getElementById('songs').appendChild(li);
+    }
+  }
+
+  window.onload = function() {
+    renderSongs();
+    renderLyrics();
+  }
+
+  // CLICK LISTENER
+  const userClick = () => {
+    if (store.getState().songsById[store.getState().currentSongId].arrayPosition === store.getState().songsById[store.getState().currentSongId].songArray.length - 1) {
+      store.dispatch({ type: 'RESTART_SONG',
+                       currentSongId: store.getState().currentSongId });
+    } else {
+      store.dispatch({ type: 'NEXT_LYRIC',
+                       currentSongId: store.getState().currentSongId });
+    }
+  }
+
+  const selectSong = (newSongId) => {
+    let action;
+    if (store.getState().currentSongId) {
+      action = {
+        type: 'RESTART_SONG',
+        currentSongId: store.getState().currentSongId
+      }
+      store.dispatch(action);
+    }
+    action = {
+      type: 'CHANGE_SONG',
+      newSelectedSongId: newSongId
+    }
+    store.dispatch(action);
+  }
+
+  // SUBSCRIBE TO REDUX STORE
+  store.subscribe(renderLyrics);
