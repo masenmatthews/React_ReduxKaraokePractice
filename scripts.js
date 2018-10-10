cf3916072816502cf36afa110b0945cc1652190e
Masen Matthews <masenmatthews@gmail.com>
2018-10-09 19:47:07 -0700

    add more songs, make main songlist

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
-  const songLyricsArray = "But I'm a constant headache, a tooth out of line They try to make you regret it You tell 'em, 'No, not this time.' I'm just a constant headache, a dead pet device You hang me up unfinished With the better part of me no longer mine".split(', ');
+  const songList = {
+    1: "But I'm a constant headache, A tooth out of line, They try to make you regret it You tell 'em, 'No not this time.', I'm just a constant headache, A dead pet device, You hang me up unfinished with the better part of me no longer mine".split(', '),
+    2: "Looking at your face in the dark, You don't even look that smart, Could never make it past that part and now I guess we never will, Looking for the keys to the truck, Your body's saying isn't that enough, Your brain is going i don't give a fuck, You treat it like a game of skill, But its more like a work of art, Or money in a christmas card, You think about it way too hard, I'm gonna stay with you until, You disappear into the crowd, I don't know what you tried to tell me, you know i think about it still".split(', '),
+  );
 
   // INITIAL REDUX STATE
   const initialState = {
@@ -8,12 +11,22 @@
 
   // REDUX REDUCER
   const reducer = (state = initialState, action) => {
+    let newState;
     switch (action.type) {
-      case 'NEXT_LYRIC':
-        let newArrayPosition = state.arrayPosition + 1;
-        let newState = {
-          songLyricsArray: state.songLyricsArray,
-          arrayPosition: newArrayPosition,
+      case 'ACTION_ONE':
+        // any necessary logic
+        newState = {
+          firstStateItem: state.firstStateItem,
+          secondStateItem: state.secondStateItem,
+          stateItemBeingChanged: action.newUpdatedStateValue,
+        }
+        return newState;
+      case 'ACTION_TWO':
+        // any necessary logic
+        newState = {
+          firstStateItem: state.firstStateItem,
+          secondStateItem: state.secondStateItem,
+          stateItemBeingChanged: action.differentUpdatedStateValue,
         }
         return newState;
       default:
@@ -31,10 +44,16 @@
     arrayPosition: 1
   });
 
+  expect(reducer({
+      songLyricsArray: songLyricsArray,
+      arrayPosition: 1,
+    },
+    { type: 'RESTART_SONG' })
+  ).toEqual(initialState);
+
   // REDUX STORE
   const { createStore } = Redux;
   const store = createStore(reducer);
-  console.log(store.getState());
 
   // RENDERING STATE IN DOM
   const renderLyrics = () => {
@@ -53,8 +72,11 @@
 
   // CLICK LISTENER
   const userClick = () => {
-    console.log('click');
-    store.dispatch({ type: 'NEXT_LYRIC'} );
+    if (store.getState().arrayPosition === store.getState().songLyricsArray.length - 1) {
+      store.dispatch({ type: 'RESTART_SONG' } );
+    } else {
+      store.dispatch({ type: 'NEXT_LYRIC' } );
+    }
   }
 
   // SUBSCRIBE TO REDUX STORE
